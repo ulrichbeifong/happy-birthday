b9204d16610ceaf489feda8c28706acda6926bf0
ulrichbeifong <nhatviet2004ks@gmail.com>
2026-04-29 01:08:32 +0700

    Update galleryData.js

--- src/data/galleryData.js
+++ src/data/galleryData.js
@@ -1,22 +1,22 @@
 export const galleryData = [
   {
     id: 1,
-    src: "",
+    src: "/images/gallery-1.jpg",
     caption: "Xinh đẹp tuyệt vời"
   },
   {
     id: 2,
-    src: "",
+    src: "/images/gallery-2.jpg",
     caption: "Đáng yêu hết phần thiên hạ"
   },
   {
     id: 3,
-    src: "",
+    src: "/images/gallery-3.jpg",
     caption: "Cute phô mai queee"
   },
   {
     id: 4,
-    src: "",
+    src: "/iimages/gallery-4.jpg",
     caption: "Oách xà lách vô cùng"
   }
 ];
